d09360ecde4e285450d3911e7772a71e39280de5
ahmadali shafiee <mail@ahmadalli.net>
2019-12-08 17:21:36 +0330

    async concurrent queue
    Signed-off-by: ahmadali shafiee <mail@ahmadalli.net>

---
+++ Collections/AsyncConcurrentQueue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ahmadalli.DotNetUtils.Collections
+{
+    public class AsyncConcurrentQueue<TEntity>
+    {
+        private readonly ConcurrentQueue<TEntity> _queue = new ConcurrentQueue<TEntity>();
+        private readonly SemaphoreSlim _countSemaphoreSlim = new SemaphoreSlim(0);
+
+        public void Enqueue(TEntity item)
+        {
+            _queue.Enqueue(item);
+            _countSemaphoreSlim.Release();
+        }
+
+        public async Task<TEntity> TryDequeueAsync(CancellationToken cancellationToken = default)
+        {
+            await _countSemaphoreSlim.WaitAsync(cancellationToken);
+            if (!_queue.TryDequeue(out var result))
+                throw new InvalidOperationException("dequeuing failed");
+            return result;
+        }
+    }
+}
