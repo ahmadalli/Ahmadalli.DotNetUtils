5112e79a13dcff14ceef76f4ab1d8a22f8f860d6
ahmadali shafiee <mail@ahmadalli.net>
2019-12-08 17:21:18 +0330

    semaphore slim storage
    Signed-off-by: ahmadali shafiee <mail@ahmadalli.net>

---
+++ Collections/SemaphoreSlimStorage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ahmadalli.DotNetUtils.Collections
+{
+    public class SemaphoreSlimStorage<TEntity>
+    {
+        private readonly int _semaphoreInitCount;
+        private readonly int _semaphoreMaxCount;
+
+        private readonly Dictionary<TEntity, SemaphoreSlim> _semaphoresDictionary = new Dictionary<TEntity, SemaphoreSlim>();
+
+        private readonly SemaphoreSlim _semaphoreDictionarySemaphore = new SemaphoreSlim(1, 1);
+
+        public SemaphoreSlimStorage(int semaphoreInitCount, int semaphoreMaxCount = int.MaxValue)
+        {
+            _semaphoreInitCount = semaphoreInitCount;
+            _semaphoreMaxCount = semaphoreMaxCount;
+        }
+
+        public async Task<SemaphoreSlim> GetSemaphore(TEntity key)
+        {
+            await _semaphoreDictionarySemaphore.WaitAsync();
+            try
+            {
+                if (!_semaphoresDictionary.ContainsKey(key))
+                    _semaphoresDictionary.Add(key, new SemaphoreSlim(_semaphoreInitCount, _semaphoreMaxCount));
+                return _semaphoresDictionary[key];
+            }
+            finally
+            {
+                _semaphoreDictionarySemaphore.Release();
+            }
+        }
+    }
+}
