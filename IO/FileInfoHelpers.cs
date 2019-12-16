b365ccad04b88f1ab44584178c8f8407654e1d2e
ahmadali shafiee <mail@ahmadalli.net>
2019-12-16 13:40:26 +0330

    check if file is locked
    Signed-off-by: ahmadali shafiee <mail@ahmadalli.net>

---
+++ IO/FileInfoHelpers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Ahmadalli.DotNetUtils.IO
+{
+    public static class FileInfoHelpers
+    {
+        public static bool IsFileLocked(this FileInfo file)
+        {
+            try
+            {
+                using var stream = file.Open(FileMode.Open, FileAccess.Read, FileShare.None);
+                stream.Close();
+            }
+            catch (IOException)
+            {
+                //the file is unavailable because it is:
+                //still being written to
+                //or being processed by another thread
+                //or does not exist
+                return true;
+            }
+
+            //file is not locked
+            return false;
+        }
+    }
+}
